3c52c1641b87758b072804406825dc8bc20b4128
Pablo Mérida Velasco <a21mevepa@iesgrancapitan.org>
2023-05-22 13:47:05 +0200

    Refactorizacion 1 Perro - Pablo Merida Velasco

--- Perro.py
+++ Perro.py
@@ -5,8 +5,12 @@ Autor: Jaime Rabasco Ronda.
 """
 class Perro:
 
+    def __init__(self):
+        self.ladra = None
+
     def ladrar(self):
-        print('Guau');
+        self.ladra = 'Guau'
+        print(self.ladra);
 
 p = Perro();
 p.ladrar();
